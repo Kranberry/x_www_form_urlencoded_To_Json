f65d72c35745efa6b404d999b9437d3d680444ef
Kranberry <andreas.b.kvist@gmail.com>
2021-06-14 11:24:49 +0200

    Added comments

--- UrlEncodedToJsonCoverter.cs
+++ UrlEncodedToJsonCoverter.cs
@@ -11,14 +11,16 @@ namespace x_www_form_urlencoded_To_Json
 {
     class UrlEncodedToJsonCoverter
     {
-        private static Regex FindKey = new("(?<=\\\")(.*?)(?=\":)");
-        private static Regex FindValue = new("(?<=\\:\")(.*?)(?=\\\")");
+        private static readonly Regex FindKey = new("(?<=\\\")(.*?)(?=\":)");
+        private static readonly Regex FindValue = new("(?<=\\:\")(.*?)(?=\\\")");
 
         public static string UrlEncodedJsonifier(string urlEncodedStr)
         {
-            StringBuilder sb = new();
-            
+            StringBuilder sb = new();            
+            // Decode the string into a "dictionary"
             NameValueCollection dict = HttpUtility.ParseQueryString(urlEncodedStr);
+
+            // Sort it up
             for (int i = 0; i < dict.AllKeys.Length; i++)
             {
                 sb.Append($"\"{dict.AllKeys[i]}\":\"{dict[dict.AllKeys[i]]}\"");
@@ -26,8 +28,10 @@ namespace x_www_form_urlencoded_To_Json
                     sb.Append(',');
                 sb.Append('\n');
             }
-
+            // Get every line, this will let us get what properties goesto waht object
             string[] rows = sb.ToString().Split('\n');
+            
+            // Build the json objects
             List<JsonObject> jsonObjects = new();
             foreach(string row in rows)
             {
@@ -35,10 +39,13 @@ namespace x_www_form_urlencoded_To_Json
                 //jsonObjects.Add(BuildObject(row));
             }
 
+            // Group every object into groups of their parent
             IEnumerable<IGrouping<string, JsonObject>> grouping = jsonObjects.GroupBy(j => j.ParentObjectName);
             List<JsonObject> joinedObjects = new();
+            // Combine each JsonObject to a singular object with the right parent as a parent.
             foreach(IGrouping<string, JsonObject> jsons in grouping)
             {
+                // If there are multiple json objects as a child under one json object, then we need to differenciate between them
                 IEnumerable<IGrouping<string, JsonObject>> groups = jsons.GroupBy(x => x.ObjectName);
 
                 foreach (IGrouping<string, JsonObject> group in groups)
@@ -65,6 +72,7 @@ namespace x_www_form_urlencoded_To_Json
 
             foreach (JsonObject obj in joinedObjects.ToList())
             {
+                // Give every JsonObject their respective child objects
                 List<JsonObject> childs = joinedObjects.Where(o => obj.ObjectName.Equals(o.ParentObjectName)).ToList();
                 childs.ForEach(o => 
                 {
@@ -72,28 +80,28 @@ namespace x_www_form_urlencoded_To_Json
                     joinedObjects.Remove(o);
                 });
             }
+
+            // Start building the Json string
             StringBuilder json = BuildJsonFromObjectList(joinedObjects);
             json.Insert(0, '{');
             json.Append('}');
 
-            //for(int i = 0; i < jsonObjects.Count; i++)
-            //{
-            //    json.Append($"\"{jsonObjects[i].PropertyName}\":\"{jsonObjects[i].Value}\"");
-            //    if (jsonObjects.Count - i != 1)
-            //        json.Append(',');
-            //}
             return json.ToString();
         }
 
+        // Build the jsonstring from a list of JsonObjects
         private static StringBuilder BuildJsonFromObjectList(List<JsonObject> objects)
         {
             StringBuilder json = new();
             for (int i = 0; i < objects.Count; i++)
             {
+                // Remove junk
                 objects[i].Properties = objects[i].Properties.Where(o => !string.IsNullOrEmpty(o.PropertyName)).ToList();
+                // Do not include the root object in the json string. Root is the very first bracket pair
                 if (!objects[i].ObjectName.Equals("Root"))
                     json.Append($"\"{objects[i].ObjectName}\":{{");
 
+                // Build the json properties as json strings
                 for (int j = 0; j < objects[i].Properties.Count; j++)
                 {
                     json.Append($"\"{objects[i].Properties[j].PropertyName}\":\"{objects[i].Properties[j].Value}\"");
@@ -104,7 +112,8 @@ namespace x_www_form_urlencoded_To_Json
                     }
                 }
 
-                json.Append(BuildJsonFromObjectList(objects[i].ChildObjects).ToString());
+                // Use recursion to do the same things for every child JsonObject
+                json.Append(BuildJsonFromObjectList(objects[i].ChildObjects));
                 if (!objects[i].ObjectName.Equals("Root"))
                     json.Append('}');
                 if (objects.Count - i != 1)
@@ -120,6 +129,7 @@ namespace x_www_form_urlencoded_To_Json
             JsonObject rootObj = new() { ParentObjectName = "Document", ObjectName = "Root", ChildObjects = new(), Properties = new() };
             jsonObjects.Add(rootObj);
 
+            // Find the raw key(including property name and object names), and the property value
             string key = FindKey.Match(rawStringRow).Value;
             string propValue = FindValue.Match(rawStringRow).Value;
             int objectCount = key.Count(c => c.Equals('['));
@@ -132,7 +142,7 @@ namespace x_www_form_urlencoded_To_Json
 
             for (int i = 0; i <= objectCount; i++)
             {
-                string shortKey;
+                string shortKey;    // shortKey is the next object, or property
                 int subCount = 0;
                 if (key[0].Equals('['))
                 {
@@ -144,15 +154,13 @@ namespace x_www_form_urlencoded_To_Json
                     subCount = 0;
                     shortKey = key.Substring(0, key.IndexOf('['));
                 }
-                key = key.Substring(shortKey.Length + subCount);
+                key = key[(shortKey.Length + subCount)..];
                 if(shortKey[^1].Equals(']'))
-                    shortKey = shortKey.Remove(shortKey.Length - 1);
+                    shortKey = shortKey.Remove(shortKey.Length - 1);    // Remove the last square bracket if it has one
 
                 if (objectCount != i)
                 {
                     // This means that it is an object, and not a property
-                    //if(shortKey == "1")
-                    //    Console.WriteLine();
                     JsonObject jsonObject = new()
                     {
                         ParentObjectName = jsonObjects.Last().ObjectName,
@@ -166,8 +174,6 @@ namespace x_www_form_urlencoded_To_Json
                 {
                     // This means that it is a property
                     JsonObject parent = jsonObjects.Last();
-                    //if(parent.ObjectName == "merges")
-                    //    Console.WriteLine();
                     parent.Properties.Add(new JsonProperty()
                     {
                         ParentName = parent.ObjectName,
